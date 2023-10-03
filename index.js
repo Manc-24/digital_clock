da6755a596b59f5bc99d4ac9a7cc5ab1903ad376
Mansi Chopkar <113710635+Manc-24@users.noreply.github.com>
2023-10-03 16:34:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const hourEle=document.getElementById("hour");
+const minuEle=document.getElementById("minute");
+const secEle=document.getElementById("second");
+const ampmEle=document.getElementById("ampm");
+
+function updateClock()
+{
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm="AM";
+
+    if(h > 12)
+    {
+        h=h-12;
+        ampm="PM";
+    }
+    
+    h=h<10 ? "0"+h : h;
+    s=s<10 ? "0"+s : s;
+    m=m<10 ? "0"+m : m;
+    
+    hourEle.innerText=h;
+    minuEle.innerText=m;
+    secEle.innerText=s;
+    ampmEle.innerText=ampm;
+
+    setTimeout(() => {
+        updateClock()
+    }, 1000);
+
+    
+}
+
+updateClock();
+
